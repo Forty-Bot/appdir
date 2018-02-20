4ed07eedf32811a1f1443d3c1164ff7eb99a2537
Sean Anderson <seanga2@gmail.com>
2018-02-19 23:37:34 -0500

    Add dummy support for other platforms

---
+++ src/dummy.c
@@ -0,0 +1,38 @@
+#include "appdir.h"
+
+#include <errno.h>
+#include <stdio.h>
+
+#ifndef ENOSYS
+#define ENOSYS 38
+#endif
+
+#define UNIMPLEMENTED errno = ENOSYS; return NULL
+
+char *appdir_data(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+char **appdir_data_dirs(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+char *appdir_cfg(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+char **appdir_cfg_dirs(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+char *appdir_cache(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+char *appdir_log(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
+
+struct appdir *appdir(const char *name, const char *author) {
+	UNIMPLEMENTED;
+}
