a3e8c27a31b7e945e47a2c1943e97ea79b322a08
Sean Anderson <seanga2@gmail.com>
2018-02-21 03:51:14 -0500

    Add windows support
    Currently test/print.c fails when running in MinGW... I will try testing
    it later on a real windows computer.

---
+++ src/windows.c
@@ -0,0 +1,140 @@
+#include "appdir.h"
+#include "internal.h"
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DIR_SEP '\\'
+
+static char *appdir_getenv(const char *name, const char *author,
+			   const char *envname) {
+	char *env;
+	char *ret;
+	char *p;
+	size_t env_len;
+	size_t author_len;
+	size_t name_len;
+	size_t ret_len;
+
+	if (!name) {
+		errno = EINVAL;
+		return NULL;
+	}
+	name_len = strlen(name);
+
+	if (!author)
+		author = name;
+	author_len = strlen(author);
+
+	env = getenv(envname);
+	if (!env || !env[0]) {
+		errno = EINVAL;
+		return NULL;
+	}
+	env_len = strlen(env);
+
+	ret_len = env_len + 1 + author_len + 1 + name_len;
+	ret = malloc(ret_len + 1);
+	if (!ret)
+		return NULL;
+	p = ret;
+
+	memcpy(p, env, env_len);
+	p += env_len;
+
+	*p++ = DIR_SEP;
+
+	memcpy(p, author, author_len);
+	p += author_len;
+
+	*p++ = DIR_SEP;
+	
+	memcpy(p, name, name_len);
+	p += name_len;
+	
+	*p = '\0';
+
+	return ret;
+}
+
+char *appdir_data(const char *name, const char *author) {
+	return appdir_getenv(name, author, "APPDATA");
+}
+
+static char **appdir_getenv_list(const char *name, const char *author,
+				 const char *const *envs, size_t n) {
+	char **dirs;
+	size_t i;
+
+	dirs = malloc(sizeof(*dirs) * n);
+	if (!dirs)
+		return NULL;
+
+	for (i = 0; i < n; i++) {
+		dirs[i] = appdir_getenv(name, author, envs[i]);
+		if (!dirs[i]) {
+			size_t j;
+			for (j = 0; j < i; j++)
+				free(dirs[j]);
+			free(dirs);
+			return NULL;
+		}
+	}
+
+	return dirs;
+}
+
+static const char *const data_dirs_env[] = {"APPDATA", "ProgramFiles"};
+char **appdir_data_dirs(const char *name, const char *author) {
+	return appdir_getenv_list(name, author, data_dirs_env, 2);
+}
+
+char *appdir_cfg(const char *name, const char *author) {
+	return appdir_data(name, author);
+}
+
+static const char *const cfg_dirs_env[] = {"APPDATA", "ProgramData"};
+char **appdir_cfg_dirs(const char *name, const char *author) {
+	return appdir_getenv_list(name, author, cfg_dirs_env, 2);
+}
+
+char *appdir_cache(const char *name, const char *author) {
+	char *ret;
+	char *data = appdir_data(name, author);
+	
+	if (!data)
+		return NULL;
+	ret = appdir_append(data, "\\Cache");
+	free(data);
+
+	return ret;
+}
+
+char *appdir_log(const char *name, const char *author) {
+	char *ret;
+	char *data = appdir_data(name, author);
+	
+	if (!data)
+		return NULL;
+	ret = appdir_append(data, "\\Logs");
+	free(data);
+
+	return ret;
+}
+
+struct appdir *appdir_get(const char *name, const char *author) {
+	struct appdir *ret;
+
+	ret = malloc(sizeof(*ret));
+	if (!ret)
+		return NULL;
+
+	if(!(ret->data_dirs = appdir_data_dirs(name, author))
+		|| !(ret->cfg_dirs = appdir_cfg_dirs(name, author))
+		|| !(ret->cache = appdir_append(ret->data_dirs[0], "\\Cache"))
+		|| !(ret->log = appdir_append(ret->data_dirs[0], "\\Logs")))
+		return NULL;
+
+	return ret;
+}
