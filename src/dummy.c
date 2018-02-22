605ba33d829d22ac0086dc12b887a75d31695d6e
Sean Anderson <seanga2@gmail.com>
2018-02-22 01:29:35 -0500

    License as LGPLv3
    This license was chosen over MIT/BSD because I would like the library
    itself to be GPL licensed. I see no reason to use a more permissive
    license here.

--- src/dummy.c
+++ src/dummy.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018 Sean Anderson
+ *
+ * This file is part of c-appdir.
+ *
+ * c-appdir is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * c-appdir is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with c-appdir. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "appdir.h"
 #include "internal.h"
 
